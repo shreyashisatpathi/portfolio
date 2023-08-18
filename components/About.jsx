a5cadb271e0e0cdbc71401e0f4f9053f56e2220d
Shreyashi <shreyashi04@gmail.com>
2023-08-18 11:56:55 +0200

    refactored the about component

--- components/About.jsx
+++ components/About.jsx
@@ -13,22 +13,19 @@ const About = () => {
           </p>
           <h2 className='py-4'>Who I Am</h2>
           <p className='py-2 text-gray-600'>
-            I specialize in building mobile responsive front-end UI applications
+            I specialize in building responsive front-end UI applications
             that connect with API’s and other backend technologies. I’m
             passionate about learning new technologies and understand there is
             more than one way to accomplish a task. Though I am most proficient
-            in building front-end applications using HTML, CSS, Javascript, and
-            React, I am a quick learner and can pick up new tech stacks as
+            in building front-end applications using React,Typescript,Next JS,Context,Redux,
+            Javascript,HTML,CSS,Tailwind, I am a quick learner and can pick up new tech stacks as
             needed. I believe that being a great developer is not using one
             specific language, but choosing the best tool for the job.
           </p>
           <p className='py-2 text-gray-600'>
-            I started web developement in 2013 managing multiple e-commerce
-            websites on CMS platforms such as WordPress, BigCommerce, and
-            Shopify. I have experience working directly with clients and taking
-            mock wireframes all the way to deployed applications. In my spare
-            time I run Code Commerce, a Youtube channel where I teach web
-            developement and various front-end technologies.
+            I started web developement in 2017 and have since worked on managing multiple e-commerce
+            websites on CMS platforms such as BigCommerce, and Shopify. I have experience working directly with clients and taking
+            mock wireframes all the way to deployed applications. 
           </p>
           <Link href='/#projects'>
             <p className='py-2 text-gray-600 underline cursor-pointer'>
